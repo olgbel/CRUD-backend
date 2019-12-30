3b2872dbfed735ee947a70040d69f1c0b8cd3397
OlgaKC <olgbelous@gmail.com>
2019-12-30 23:47:39 +0300

    add fcm service

--- src/main/kotlin/ru/netology/service/JWTTokenService.kt
+++ src/main/kotlin/ru/netology/service/JWTTokenService.kt
@@ -4,7 +4,6 @@ import com.auth0.jwt.JWT
 import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
 
-
 class JWTTokenService(private val secret: String) {
 //    private val secret = "5c2dbef6-289c-46e6-8cfd-d8b3292d373a"
     private val algo = Algorithm.HMAC256(secret)
